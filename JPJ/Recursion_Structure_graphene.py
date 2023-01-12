50b79422e789067c04ab1e22eb7dd0bf4bb4d171
RomeoVey <rveyry@gmail.com>
2023-01-12 16:26:52 +0100

    fonction energie et PDF

--- JPJ/Recursion_Structure_graphene.py
+++ JPJ/Recursion_Structure_graphene.py
@@ -100,6 +100,7 @@ plt.plot(energy,dos)
 plt.grid()
 plt.show() # trace le graphique
 
-
-
-# je modifie 
+def energy(kx,ky,t,a):
+    e_plus = t*sqrt(1 + 4*(cos(ky*a*sqrt(3)/2)**2 + cos(kx*3*a/2)*cos(ky*a*sqrt(3)/2)))
+    e_moins = - e_plus
+    return e_plus,e_moins
